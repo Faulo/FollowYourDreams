9f8109d9b4310da1a403c9afaf1ffbbb2d3a5032
Daniel Schulz <faulolio@gmail.com>
2023-03-14 20:39:57 +0100

    Revert tile bug

---
+++ Assets/Scripts/Runtime/Level/ScriptableTile.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace FollowYourDreams.Level {
+    [CreateAssetMenu]
+    abstract class ScriptableTile : TileBase {
+        [Header("Base Tile")]
+        [SerializeField]
+        GameObject prefab = default;
+        [SerializeField]
+        Color tint = Color.white;
+
+        public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData) {
+            tileData.color = tint;
+            tileData.flags = TileFlags.LockColor | TileFlags.LockTransform;
+            tileData.colliderType = Tile.ColliderType.None;
+            tileData.gameObject = prefab;
+        }
+
+        public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go) {
+            if (go) {
+                go.transform.localRotation = prefab.transform.localRotation;
+            }
+            return true;
+        }
+
+        public override void RefreshTile(Vector3Int position, ITilemap tilemap) {
+            tilemap.RefreshTile(position);
+        }
+    }
+}
