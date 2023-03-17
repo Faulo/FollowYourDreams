e2a6905a83756f33c352a16f97d4a008c98a64c7
Daniel Schulz <faulolio@gmail.com>
2023-03-17 18:24:48 +0100

    Refactor

--- Assets/Scripts/Runtime/Level/AutoPickUp.cs
+++ Assets/Scripts/Runtime/Level/AutoPickUp.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 using UnityEngine.Events;
 
 namespace FollowYourDreams.Level {
-    sealed class PowerUpPickUp : MonoBehaviour, ICollidable {
-        [SerializeField]
-        Power powerToUnlock;
+    sealed class AutoPickUp : MonoBehaviour, ICollidable {
         [SerializeField]
         EventReference onCollectAudio = new();
         [SerializeField]
         UnityEvent<Transform> onCollect = new();
 
         public void OnCollide(ControllerColliderHit hit, AvatarController avatar) {
-            avatar.GainPower(powerToUnlock);
             onCollectAudio.PlayOnce();
             onCollect.Invoke(transform);
             gameObject.SetActive(false);
