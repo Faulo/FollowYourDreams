3f9b19164603c025b24b4576841bc552257e0c4b
Daniel Schulz <faulolio@gmail.com>
2023-03-18 13:55:29 +0100

    Fix compile errors

--- Assets/Scripts/Runtime/Level/SpriteManager.cs
+++ Assets/Scripts/Runtime/Level/SpriteManager.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using FollowYourDreams.Graphics;
+#if UNITY_EDITOR
 using Slothsoft.UnityExtensions.Editor;
+#endif
 using UnityEngine;
 using UnityObject = UnityEngine.Object;
 
